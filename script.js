e85d24303502e125bc55e928dd114216189fe6c8
Pc-Kirui <patokirui@gmail.com>
2025-07-22 22:56:20 +0300

    modified js file

--- script.js
+++ script.js
@@ -14,6 +14,34 @@ function Gameboard() {
 
   const getBoard = () => board;
 
-  return { getBoard };
+  const placeMark = (row, column, playerMark) => {
+    if (row < 0 || row >= rows || column < 0 || column >= columns) {
+      console.log("Invalid row or column. Please choose within 0-2.");
+      return false;
+    }
+
+    if (board[row][column].getValue() !== "") {
+      console.log("That cell is already taken. Please choose another.");
+      return false;
+    }
+
+    board[row][column].addMark(playerMark);
+    return true;
+  };
+
+  return { getBoard, placeMark };
 }
+
+function Cell() {
+  let value = "";
+
+  const addMark = (playerMark) => {
+    value = playerMark;
+  };
+
+  const getValue = () => value;
+
+  return { addMark, getValue };
+}
+
 Gameboard();
